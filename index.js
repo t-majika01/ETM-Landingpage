2bd86717c8dc02f2f604d95addb6b991f804d74a
Milly-01 <workthabelowork@gmail.com>
2023-06-22 19:31:15 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const myToogle = document.querySelector(".toggle_btn");
+const myToogleIcon = document.querySelector(".toggle_btn i");
+const dropDown = document.querySelector(".dropdown_menu");
+
+
+function handleClick(){
+    dropDown.classList.toggle("open");
+}
+
+
